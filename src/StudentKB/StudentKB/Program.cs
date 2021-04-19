a82b289374a094e5de49ec445170bd2d32033e0c
stu-pksp117 <stu-pksp117@hq.corp.vlsu.ru>
2021-04-19 13:28:19 +0300

    start_in

---
+++ src/StudentKB/StudentKB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace StudentKB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nRecord;
+            bool tryC;
+            do
+            {
+                Console.Write("Введите количество записей:");
+                tryC = Int32.TryParse(Console.ReadLine(), out nRecord);
+                if (tryC == false || nRecord <= 0)
+                {
+                    tryC = false;
+                    Console.Write("Введёно не верное значение. Введите целое не отрицательное число");
+                }
+            }
+            while (!tryC);
+            Student[] student;
+        }
+    }
+}
